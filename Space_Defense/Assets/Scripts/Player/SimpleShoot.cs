7f1574c2ca21973263c4573277f04238c6c0dcea
Zer0dot <Zer0dotU@gmail.com>
2017-03-28 16:42:20 -0400

    Began Project
    Began the Unity project development.

---
+++ Space_Defense/Assets/Scripts/Player/SimpleShoot.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleShoot : MonoBehaviour {
+	[SerializeField]GameObject explosion;
+
+
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition + new Vector3 (Random.Range (-7f, 7f), Random.Range (-7f, 7f), Random.Range (-7f, 7f)));
+			//Collision
+			RaycastHit hit;
+			if (Physics.Raycast (ray, out hit)) {
+				GameObject createdExplosion = Instantiate (explosion, hit.point, Quaternion.identity) as GameObject;
+				createdExplosion.SetActive (true);
+			}
+		}
+	}
+}
